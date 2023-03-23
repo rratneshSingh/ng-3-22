468d8c9d5612b5987abdb026a8a06d8683a9cbc7
ratnesh singh <dev.ratnesh.singh@gmail.com>
2023-03-23 20:00:54 +0530

    23-3-23 Property Binding, Event Binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'demo';
+  name = 'Random'
+
+  displaySomething(e: any) {
+    debugger;
+    console.log(e)
+  }
 }
